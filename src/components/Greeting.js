7d9a4a19c8db9cb2206937ec26c64bd1d6a16132
luidimso <luidi_mso@outlook.com>
2023-12-07 15:43:54 -0300

    Testing click event

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,8 +1,17 @@
+import { useState } from "react";
+
 const Greeting = () => {
+    const [text, setText] = useState(false);
+
+    function changeTest() {
+        setText(true);
+    }
+
     return (
         <div>
             <h2>Hello</h2>
-            <p>Testing</p>
+            <p>{text ? "Tested" : "Testing"}</p>
+            <button onClick={changeTest}></button>
         </div>
     );
 };
--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting";
+import userEvent from "@testing-library/user-event";
 
 test("should display Hello text", () => {
     render(<Greeting></Greeting>);
@@ -7,4 +8,18 @@ test("should display Hello text", () => {
         exact: true
     });
     expect(test).toBeInTheDocument();
+});
+
+test("should change page text when button clicked", () => {
+    render(<Greeting></Greeting>);
+    const button = screen.getByRole("button");
+    userEvent.click(button);
+    const text = screen.getByText("Tested")
+    expect(text).toBeInTheDocument();
+});
+
+test("should display expected page test", () => {
+    render(<Greeting></Greeting>);
+    const text = screen.getByText("Testing")
+    expect(text).toBeInTheDocument();
 });
